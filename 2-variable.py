dc90f607a9b87c2a963f63461de691a7000037f3
Tushar Shejwal <tusharshejwal1803@gmail.com>
2025-12-01 20:46:29 +0530

    Add files via upload

---
+++ 2-variable.py
@@ -0,0 +1,25 @@
+#What is variable??
+
+#Variable is a container which hold a value of a data type.
+
+a=20 #Integer
+
+name="Jay" #String
+
+print('My name is',name)
+print('My age is',a)
+
+
+firstname="Tushar Shejwal"
+city="Patan"
+collage="DY Patil College,Akurdi"
+age=23
+
+
+print('My name is',firstname,
+      'My city is',city,
+      'My collage is',collage,
+      'My age is',age)
+
+
+
