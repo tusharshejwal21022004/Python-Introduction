39abe8e0481ab0e3a4b8db40e866b305ae1cb047
Tushar Shejwal <tusharshejwal1803@gmail.com>
2025-12-01 20:47:40 +0530

    Add files via upload

---
+++ 6-inbuilt_DS.py
@@ -0,0 +1,18 @@
+'''
+1.Numaric Data Type:
+--- int eg: 23,78,90,0
+--- float eg:34.78,90.00
+--- complex eg: 1+7j
+2.Sequence data type:
+--- list eg. l=[12,78.0,True,1+7j]
+--- tuple eg. l=(12,7.9,"jay")
+--- range eg. range(5) 
+--- string eg: "jay"
+3.Set data type:
+--- set eg: s={12,"trigger"}
+--- frozen set: s={1,2,3,4}
+4.Maping data type:
+--- Dictionary: eg: d={"key":"val"}
+5.Boolean data type:
+--- True False
+'''
