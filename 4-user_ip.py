3408f63b607c1a141b07e01fb92d5a37b2fa4e00
Tushar Shejwal <tusharshejwal1803@gmail.com>
2025-12-01 20:46:57 +0530

    Add files via upload

---
+++ 4-user_ip.py
@@ -0,0 +1,15 @@
+#User Input:
+
+# name=input("Enter your name:")
+# print(name)
+
+
+name=input("Enter your name: ")
+age=int(input("Enter your age: "))
+collage=input("Enter your collage name: ")
+
+print("My name is",name,
+      "My age is", age,
+      "My Collage is", collage )
+
+print(f'my name is {name}, my age is {age}, my collage is {collage}')
