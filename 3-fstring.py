eb33b77360a43e965cddec18c20f6708585b7d90
Tushar Shejwal <tusharshejwal1803@gmail.com>
2025-12-01 20:46:42 +0530

    Add files via upload

---
+++ 3-fstring.py
@@ -0,0 +1,13 @@
+# F-string:
+
+a=10
+b=20
+
+print(f"The value of a is {a} and b is {b}")
+
+name="Tushar Shejwal"
+city="Patan"
+collage="DY Patil College,Akurdi"
+age=23
+
+print(f"My name is {name}, City is {city}, My College is {collage}, & my age is {age} ")
