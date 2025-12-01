6ed9d6cc2a23554f0b453d9199f0021c1144f948
Tushar Shejwal <tusharshejwal1803@gmail.com>
2025-12-01 20:46:17 +0530

    Add files via upload

---
+++ 1-first_prog.py
@@ -0,0 +1,23 @@
+# First program:
+
+print("Codenera")
+
+print('Python')
+
+print('''
+    My name is Tushar
+      hello this is 1st class
+      ''')
+
+print('''
+    Name:Tushar
+    City:Patan
+    State:Maharashtra
+    Address: At Post Patan
+     Tal.Patan Dist.Satara
+     Pincode :415205
+    Graduation : Bsc in Computer Science
+    From : Karad
+''')
+
+
