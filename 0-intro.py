33d7a38e97a91a28d1946a90127c12a4dc8e5f39
Tushar Shejwal <tusharshejwal1803@gmail.com>
2025-12-01 20:46:03 +0530

    Add files via upload

---
+++ 0-intro.py
@@ -0,0 +1,31 @@
+#What is Python?
+
+'''
+1.Python has simple and clear syntax.
+ This syntax in same as a english language. So 
+
+2.Python is a Interpreted Language.
+ It execute each statement line by line.
+                       
+3.Python is a Dynamically typed language.
+  In it the datatype has check during the exucetion we dont want to declare the variable types. 
+
+4.Python is a Open source language.
+ We uses no cost for it and anyone can use python.
+             
+5.Python is a Object Oriented Language.
+ 
+6.Python has rich ecos-system and libraries.
+   Web dev - Djano and Flask Framework
+   DS - pandas    
+
+7.Python is High level Programming language.
+   easy for human read and write.
+ 
+8.Python is the platform independent language.
+ we can use on any operating system.
+
+9.Garbage Collection.
+ Garbage collection is automatic process collection that are no longer in use.
+ 
+'''
