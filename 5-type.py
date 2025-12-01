5aea552ec036b74ae96af53fa30d405a2fb86105
Tushar Shejwal <tusharshejwal1803@gmail.com>
2025-12-01 20:47:11 +0530

    Add files via upload

---
+++ 5-type.py
@@ -0,0 +1,36 @@
+# Type()
+
+# It will return data type of specified data
+
+a=10  #Int
+print(type(a))
+
+a="jay"  #string
+print(type(a))
+
+a=10.20  #float
+print(type(a))
+
+a=10
+b=20
+print(a+b)  #Addition
+
+a="10"
+b="20"
+print(a+b)  #Concat
+
+
+
+#Conversion of int to float and string
+
+a=12
+res=float(a)
+print(res)
+
+
+a=12
+res=str(a)
+print(res)
+print(type(res))
+
+
